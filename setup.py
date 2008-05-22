a557d2f2089f564fe4b1fffa101a8517e1f1b5bd
scott@quadhome.com <scott@quadhome.com@825e4504-c94d-0410-9260-67b28d91b2a4>
2008-05-22 03:13:42 +0000

    setuptools base.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+  name = 'bzrshelve',
+  version = '0.1',
+  packages = find_packages(),
+
+  install_requires = [
+    'bzr',
+  ],
+
+  test_suite = 'nose.collector'
+)
