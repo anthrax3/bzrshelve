89c3a29094375003e1938e1f7606827004721970
scott@quadhome.com <scott@quadhome.com@825e4504-c94d-0410-9260-67b28d91b2a4>
2008-05-25 23:26:57 +0000

    PEP-8 formatting. Moved RoundtripTest data population into its own function.

--- bzrshelve-test.py
+++ bzrshelve-test.py
@@ -12,190 +12,191 @@ test_data = {'str': 'abc123', 'num': 4321, 'float': 1.234,
              'list': list(), 'dict': dict(), 'tuple': tuple()}
 
 class OpenCloseTests(unittest.TestCase):
-  """Tests for open and close operations."""
+    """Tests for open and close operations."""
 
-  def setUp(self):
-    self.workdir = tempfile.mkdtemp()
+    def setUp(self):
+        self.workdir = tempfile.mkdtemp()
 
-  def tearDown(self):
-    shutil.rmtree(self.workdir)
+    def tearDown(self):
+        shutil.rmtree(self.workdir)
 
-  def test_open_new(self):
-    """Create a new shelf."""
+    def test_open_new(self):
+        """Create a new shelf."""
 
-    shelf = bzrshelve.open(self.workdir)
-    assert shelf is not None
+        shelf = bzrshelve.open(self.workdir)
+        assert shelf is not None
 
-  def test_open_nonexistent(self):
-    """Open shelf in a non-existent directory."""
+    def test_open_nonexistent(self):
+        """Open shelf in a non-existent directory."""
 
-    fakedir = tempfile.mkdtemp()
-    os.rmdir(fakedir)
+        fakedir = tempfile.mkdtemp()
+        os.rmdir(fakedir)
 
-    self.assertRaises(bzrlib.errors.NoSuchFile,
-                      bzrshelve.open, os.path.join(fakedir, 'a'))
+        self.assertRaises(bzrlib.errors.NoSuchFile,
+                          bzrshelve.open, os.path.join(fakedir, 'a'))
 
-  def test_close_new(self):
-    """Close an empty shelf."""
+    def test_close_new(self):
+        """Close an empty shelf."""
 
-    assert not bzrshelve.open(self.workdir).close()
+        assert not bzrshelve.open(self.workdir).close()
 
 class DictionaryTests(OpenCloseTests):
-  """Tests for dictionary operations."""
+    """Tests for dictionary operations."""
 
-  def setUp(self):
-    OpenCloseTests.setUp(self)
-  
-    self.shelf = bzrshelve.open(self.workdir)
+    def setUp(self):
+        OpenCloseTests.setUp(self)
+      
+        self.shelf = bzrshelve.open(self.workdir)
 
-  def tearDown(self):
-    self.shelf.close()
+    def tearDown(self):
+        self.shelf.close()
 
-    OpenCloseTests.tearDown(self)
+        OpenCloseTests.tearDown(self)
 
-  def test_set(self):
-    """Set a key."""
+    def test_set(self):
+        """Set a key."""
 
-    self.shelf['foobar'] = 'abc123'
+        self.shelf['foobar'] = 'abc123'
 
-    self.assertEqual('abc123', self.shelf['foobar'])
+        self.assertEqual('abc123', self.shelf['foobar'])
 
-  def test_delete(self):
-    """Set and delete a key."""
+    def test_delete(self):
+        """Set and delete a key."""
 
-    self.shelf['foobar'] = 'abc123'
-    del self.shelf['foobar']
+        self.shelf['foobar'] = 'abc123'
+        del self.shelf['foobar']
 
-    def test():
-      return self.shelf['foobar']
+        def test():
+            return self.shelf['foobar']
 
-    self.assertRaises(KeyError, test)
+        self.assertRaises(KeyError, test)
 
-  def test_overwrite_value_short(self):
-    """Overwrite a key with a shorter value."""
+    def test_overwrite_value_short(self):
+        """Overwrite a key with a shorter value."""
 
-    self.shelf['foobar'] = 'abc123'
-    self.shelf['foobar'] = 'a'
+        self.shelf['foobar'] = 'abc123'
+        self.shelf['foobar'] = 'a'
 
-    self.assertEqual('a', self.shelf['foobar'])
+        self.assertEqual('a', self.shelf['foobar'])
 
-  def test_overwrite_value_long(self):
-    """Overwrite a key with a longer value."""
+    def test_overwrite_value_long(self):
+        """Overwrite a key with a longer value."""
 
-    self.shelf['foobar'] = 'abc123'
-    self.shelf['foobar'] = 'abc1234'
+        self.shelf['foobar'] = 'abc123'
+        self.shelf['foobar'] = 'abc1234'
 
-    self.assertEqual('abc1234', self.shelf['foobar'])
+        self.assertEqual('abc1234', self.shelf['foobar'])
 
-  def test_overwrite_value_short_synced(self):
-    """Overwrite a key with a shorter value. (synced)"""
+    def test_overwrite_value_short_synced(self):
+        """Overwrite a key with a shorter value. (synced)"""
 
-    self.shelf['foobar'] = 'abc123'
-    self.shelf.sync()
+        self.shelf['foobar'] = 'abc123'
+        self.shelf.sync()
 
-    self.shelf['foobar'] = 'a'
-    self.shelf.sync()
+        self.shelf['foobar'] = 'a'
+        self.shelf.sync()
 
-    self.assertEqual('a', self.shelf['foobar'])
+        self.assertEqual('a', self.shelf['foobar'])
 
-  def test_overwrite_value_long_synced(self):
-    """Overwrite a key with a longer value. (synced)"""
+    def test_overwrite_value_long_synced(self):
+        """Overwrite a key with a longer value. (synced)"""
 
-    self.shelf['foobar'] = 'abc123'
-    self.shelf.sync()
+        self.shelf['foobar'] = 'abc123'
+        self.shelf.sync()
 
-    self.shelf['foobar'] = 'abc1234'
-    self.shelf.sync()
+        self.shelf['foobar'] = 'abc1234'
+        self.shelf.sync()
 
-    self.assertEqual('abc1234', self.shelf['foobar'])
+        self.assertEqual('abc1234', self.shelf['foobar'])
 
-  def test_set_delete_set(self):
-    """Repeated set and delete a key."""
+    def test_set_delete_set(self):
+        """Repeated set and delete a key."""
 
-    def test():
-      return self.shelf['foobar']
+        def test():
+            return self.shelf['foobar']
 
-    self.shelf['foobar'] = 'abc123'
-    self.assertEqual('abc123', self.shelf['foobar'])
+        self.shelf['foobar'] = 'abc123'
+        self.assertEqual('abc123', self.shelf['foobar'])
 
-    del self.shelf['foobar']
-    self.assertRaises(KeyError, test)
+        del self.shelf['foobar']
+        self.assertRaises(KeyError, test)
 
-    self.shelf['foobar'] = 'abc'
-    self.assertEqual('abc', self.shelf['foobar'])
+        self.shelf['foobar'] = 'abc'
+        self.assertEqual('abc', self.shelf['foobar'])
 
-    del self.shelf['foobar']
-    self.assertRaises(KeyError, test)
+        del self.shelf['foobar']
+        self.assertRaises(KeyError, test)
 
-  def test_keys_index(self):
-    """Get a list of keys."""
+    def test_keys_index(self):
+        """Get a list of keys."""
 
-    keys = ['abc', '123', 'longishstringnamewith somet places in it']
+        keys = ['abc', '123', 'longishstringnamewith somet places in it']
 
-    for key in keys:
-      self.shelf[str(key)] = key
+        for key in keys:
+            self.shelf[str(key)] = key
 
-    for key in keys:
-      self.assertTrue(key in self.shelf)
-      self.assertTrue(key in self.shelf.keys())
+        for key in keys:
+            self.assertTrue(key in self.shelf)
+            self.assertTrue(key in self.shelf.keys())
 
-  def test_keys_index_synced(self):
-    """Get a list of keys. (synced)"""
+    def test_keys_index_synced(self):
+        """Get a list of keys. (synced)"""
 
-    keys = ['abc', '123', 'longishstringnamewith somet places in it']
+        keys = ['abc', '123', 'longishstringnamewith somet places in it']
 
-    for key in keys:
-      self.shelf[str(key)] = key
-      self.shelf.sync()
+        for key in keys:
+            self.shelf[str(key)] = key
+            self.shelf.sync()
 
-    for key in keys:
-      self.assertTrue(key in self.shelf)
-      self.assertTrue(key in self.shelf.keys())
+        for key in keys:
+            self.assertTrue(key in self.shelf)
+            self.assertTrue(key in self.shelf.keys())
 
-  def test_update(self):
-    """Use the dictionary update interface."""
+    def test_update(self):
+        """Use the dictionary update interface."""
 
-    self.shelf.update(test_data)
-    self.shelf.sync()
+        self.shelf.update(test_data)
+        self.shelf.sync()
 
-    for key, value in test_data.iteritems():
-      self.assertEquals(value, self.shelf[key])
+        for key, value in test_data.iteritems():
+            self.assertEquals(value, self.shelf[key])
 
 class RoundtripTests(OpenCloseTests):
-  def setUp(self):
-    OpenCloseTests.setUp(self)
+    def populateShelf(self, shelf):
+        # Fill the shelf with test data.
+        shelf.update(test_data)
+        shelf.sync()
 
-    # Open a new shelf.
-    shelf = bzrshelve.open(self.workdir)
+        # Create and delete a key.
+        shelf['foobar'] = 'abc123'
+        del shelf['foobar']
 
-    # Fill the shelf with test data.
-    shelf.update(test_data)
-    shelf.sync()
+    def setUp(self):
+        OpenCloseTests.setUp(self)
 
-    # Create and delete a key.
-    shelf['foobar'] = 'abc123'
-    del shelf['foobar']
+        # Populate a new shelf.
+        shelf = bzrshelve.open(self.workdir)
+        self.populateShelf(shelf)
+        shelf.close()
 
-    shelf.close()
+        # Reopen the shelf.
+        self.shelf = bzrshelve.open(self.workdir)
 
-    # Reopen the shelf for testing.
-    self.shelf = bzrshelve.open(self.workdir)
+    def tearDown(self):
+        self.shelf.close()
 
-  def tearDown(self):
-    self.shelf.close()
+        OpenCloseTests.tearDown(self)
 
-    OpenCloseTests.tearDown(self)
+    def test_check_data(self):
+        """Reload test data."""
 
-  def test_check_data(self):
-    """Reload test data."""
+        for key, value in test_data.iteritems():
+            self.assertEquals(value, self.shelf[key])
 
-    for key, value in test_data.iteritems():
-      self.assertEquals(value, self.shelf[key])
+    def test_deleted_data(self):
+        """Check if deleted keys are gone."""
 
-  def test_deleted_data(self):
-    """Check if deleted keys are gone."""
+        def test():
+            return self.shelf['foobar']
 
-    def test():
-      return self.shelf['foobar']
-
-    self.assertRaises(KeyError, test)
+        self.assertRaises(KeyError, test)
